fb5a6b7be3d688edd189c2dd9b7ca6dc3dc9ca40
Nicholas Trindade <nmd.trindade@gmail.com>
2022-05-04 10:15:54 -0700

    finished intial authorization backend

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
 const express = require("express");
-const app = express()
-const cookieSession = require('cookie-session');
-const keys = require('./config/keys');
-const passport = require('passport');
-const mongoose = require('mongoose');
-require('./schemas/user');
-require('./services/passport');
+const app = express();
+const cookieSession = require("cookie-session");
+const keys = require("./config/keys");
+const passport = require("passport");
+const mongoose = require("mongoose");
+require("./schemas/user");
+require("./services/passport");
 
 
-
-
-mongoose.connect(keys.mongoDb);
-
+mongoose.connect(keys.mongoDb)
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
 //Cookie data
-app.use(cookieParser());
-app.use(cookieSession({
+app.use(
+  cookieSession({
     keys: [keys.cookieKey],
-    maxAge: 24 * 60 * 60 * 1000 // 24 hours
-}));
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+  })
+);
 //Tell passport to use cookies
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-require('./routes/authRoutes')(app);
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/',(req,res)=>{
-    res.send("Hello setup");
+app.get("/", (req, res) => {
+  res.send("Hello setup");
 });
 
 app.listen(PORT);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,31 @@
-const passport = require('passport');
+const passport = require("passport");
 
+module.exports = (app) => {
+  app.get(
+    "/auth/google",
+    passport.authenticate("google", { scope: ["profile","email"] })
+  );
 
-module.exports = app => {
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google", { failureRedirect: "/auth/google" }),
+    (req, res) => {
+      res.redirect("/");
+    }
+  );
 
-    app.get('/auth/google', passport.authenticate('google', {scope: ['profile','email']}));
+  app.get('/api/current_user', (req,res)=>{
+    if(req.user){
+      res.send(req.user);
+    }
+    else{
+      res.send("No User Logged In");
+    }
+  })
 
-    app.get('/auth/google/callback', passport.authenticate('google', {failureRedirect:'/auth/google'}), (req,res)=>{
-        res.redirect('/');
-    });
+  app.get('/api/logout',(req,res)=>{
+    req.logout();
+    res.redirect('/');
+  });
 
 };
